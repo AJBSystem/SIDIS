96463bad8379de3a59fceeb0d62dcd46433f3292
C4R4C4S <darlynmartinezb@gmail.com>
2018-01-15 13:33:48 -0430

    Ajuste de categorias

--- modal/registro_categorias.php
+++ modal/registro_categorias.php
@@ -3,23 +3,31 @@
 		{
 	?>
 	<!-- Modal -->
-	<div class="modal fade" id="nuevoCliente" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+
+		<div class="modal fade" id="nuevoCliente" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 	  <div class="modal-dialog" role="document">
 		<div class="modal-content">
 		  <div class="modal-header">
 			<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			<h4 class="modal-title" id="myModalLabel"><i class='glyphicon glyphicon-edit'></i> Agregar nueva categoría</h4>
+			<h4 class="modal-title" id="myModalLabel"><i class='glyphicon glyphicon-edit'></i> Agregar nuevo categoria</h4>
 		  </div>
+
+
 		  <div class="modal-body">
 			<form class="form-horizontal" method="post" id="guardar_categoria" name="guardar_categoria">
 			<div id="resultados_ajax"></div>
 			  <div class="form-group">
+
+
+
 				<label for="nombre" class="col-sm-3 control-label">Nombre</label>
 				<div class="col-sm-8">
-				  <input type="text" class="form-control" id="nombre" name="nombre" required>
+				  <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre de la categoria" required>
 				</div>
 			  </div>
-			 
+
+
+
 				  
 			  <div class="form-group">
 				<label for="descripcion" class="col-sm-3 control-label">Descripción</label>
@@ -30,11 +38,8 @@
 			  </div>
 			  
 	 
-			 
-			 
-			
-		  </div>
-		  <div class="modal-footer">
+			   </div>
+			 <div class="modal-footer">
 			<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
 			<button type="submit" class="btn btn-primary" id="guardar_datos">Guardar datos</button>
 		  </div>
@@ -45,3 +50,5 @@
 	<?php
 		}
 	?>
+			
+		
