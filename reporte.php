b14dd9418b87a4713897541d1a660c0efda1b7c2
C4R4C4S <darlynmartinezb@gmail.com>
2018-01-22 08:38:26 -0430

    Se modificaron los botones

--- reporte.php
+++ reporte.php
@@ -33,22 +33,21 @@
     
   
      <div class="form-group">
-      <label for="mod_serial" class="btn btn-success btn-lg" data-toggle="modal" data-target="" >Reporte por Serial</label> 
+      <label style="background:#00b3b3" for="mod_serial" class="btn btn-info btn-lg" data-toggle="modal" data-target=""><i class='glyphicon glyphicon-filter'></i>Reporte por Serial</label> 
       <div class="col-sm-3"><a href="/SIDIS/pdf/reporteserial.php">
-         <input type="text" class="form-control" id="mod_serial" name="mod_serial" placeholder="Ingrese el serial del producto" required>
+         <input type="text" class="form-control" id="mod_serial" name="mod_serial" placeholder="Ingrese el serial del producto" right=required>
           <input type="hidden" name="mod_id" id="mod_id">
         </div>
         </div>
 
-
-
        <p><div class="btn-group pull-justify"> <a href="/SIDIS/pdf/reportegeneral.php">
-        <button  type='button' class="btn btn-success btn-lg" data-toggle="modal" data-target="">Reporte General</button>
+        <button  style="background:#00b3b3" type='button' class="btn btn-info btn-lg" data-toggle="modal" data-target=""><i class=' glyphicon glyphicon-paste'></i>Reporte General</button>
       </div></p>
 
         <p><div class="btn-group pull-justify"><a href="/SIDIS/pdf/reportearea.php">
-        <button href="#" type='button' class="btn btn-success btn-lg" data-toggle="modal" data-target="">Reporte de Área</button>
+        <button style="background:#00b3b3" href="#" type='button' class="btn btn-info btn-lg" data-toggle="modal" data-target=""><i class='glyphicon glyphicon-open-file'></i>Reporte de Área</button>
       </div></p>
+
 
 
       </div>
