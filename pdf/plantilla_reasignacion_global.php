57571248eca14113324a737d5f7dc4fcfc790dab
Arnaldo <bonillaarnaldo@hotmail.com>
2018-05-28 16:37:25 -0400

    agregado el reporte global de reasignaciones

--- pdf/plantilla_reasignacion_global.php
+++ pdf/plantilla_reasignacion_global.php
@@ -6,31 +6,37 @@ class PDF extends FPDF
 
 	function Header()
 	{
-		$this->Image('../img/logo-Ministerio.jpg', 35,15,30);
-		$this->Image('../img/cicpc_logo_trns.png', 480,5,30);
+		$this->Image('../img/logo-Ministerio.jpg', 15,13,30);
+		$this->Image('../img/cicpc_logo_trns.png', 170,5,30);
 
-		$this->SetFont('Arial','B',10);
-
+		$this->SetFont('Arial','B');
+		
 		$this->Cell(30);
-		$this->SetX(100);
-		$this->Cell(290,10,utf8_decode ('ACTA DE REASIGNACIÓN DE BIENES NACIONALES'),0,0,'C');
+		$this->SetX(10);
+		$this->Cell(200,30,utf8_decode ('ACTA DE REASIGNACIÓN DE BIENES NACIONALES'),0,0,'C');
 		$this->Ln(5);
 
 		$this->Ln(10);
 		$this->Cell(30);
 		$this->SetX(100);
-		$this->Cell(290,10,utf8_decode ('Reporte de Reasignación'),2,1,'C');
+		$this->Cell(25,60,utf8_decode ('Reporte de Reasignación'),2,1,'C');
 		$this->Ln(20);
 
-				$this->SetX(135);
-				$this->Cell(865,10,utf8_decode('En la ciudad de Caracas, se llevó a cabo por parte de la división de sistemas la entrega a la: de los bienes muebles que se especifican a continuación'),0,0,'L');
+				$this->SetX(15);
+				$this->Cell(800,-85,utf8_decode('En la ciudad de Caracas, se llevó a cabo por parte de la división de sistemas la entrega a la: de los bienes muebles que se especifican a continuación'),0,0,'L');
 				$this->Ln(10);
 				
 
 				
-				$this->SetY(300);
-				$this->SetX(80);
-				$this->Cell(420,10,utf8_decode('Haciendo constar que dichos activos se encuentran en óptimas condiciones físicas. Se levanta la presente acta y en señal de conformidad firma el funcionario receptor, quedando este responsable del uso, conservación y custodia de estos bienes: '),0,0,'R');
+				$this->SetY(220);
+				$this->SetX(75);
+				$this->Cell(420,10,utf8_decode('Haciendo constar que dichos activos se encuentran en óptimas condiciones físicas. Se levanta la presente acta y en señal de conformidad firma el funcionario receptor, quedando este responsable del uso, '),0,0,'R');
+				$this->Ln(10);
+				
+
+				$this->SetX(10);
+				$this->Cell(155,10,utf8_decode('conservación y custodia de estos bienes: '),0,0,'R');
+
 				$this->Ln(15);
 				$this->Cell(175,10,utf8_decode('Recibe Conforme'),0,0,'C');
 				$this->Ln(10);
--- pdf/reporte_reasignacion_global.php
+++ pdf/reporte_reasignacion_global.php
@@ -1,31 +1,21 @@
 <?php
   extract($_GET);
-  include('plantilla_reasignacion.php');
+  include('plantilla_reasignacion_global.php');
   require('conexion.php');
   
-  $id_producto = $_GET['id_producto'];
-  $query = "SELECT fecha_products, codigo_producto, serial,  nombre_producto, `marca_producto`,modelo_producto , categorias.nombre_categoria , numero_bien, motivo.nombre_motivo `nombre_motivo`, `condicion_producto`, `responsable_entrega`, `asignacion_producto`, `codigo_inventario`, `precio_producto` ,stock FROM `products` 
-  INNER JOIN categorias on products.id_categoria = categorias.id_categoria  
-  INNER JOIN motivo on products.id_motivo = motivo.id_motivo 
-  WHERE id_producto = $id_producto";
-
+  $query="SELECT * FROM `products` WHERE asignacion_producto = 'Anthony Benitez'";             
+  
   $resultado = $mysqli->query($query);
 
-  $pdf = new PDF('L', 'mm', array(600,500));
+  $pdf = new PDF('L', 'mm', array(400,300));
   $pdf->AliasNbPages();
   $pdf->AddPage();
-
-
-  // AQUI VA LA FOTO//
-  $pdf->SetFillColor(80,150,200);
-  $pdf->Rect(80,70,100,100,'F');
-  /// FIN FOTO//
 
   $pdf->SetFillColor(232,232,232);
   $pdf->SetFont('Arial','B',12);
   
   $pdf->SetY(70);
-  $pdf->SetX(180);
+  $pdf->SetX(30);
   $pdf->Cell(30,6,utf8_decode('Cod. Bien'),1,0,'C',1);
   $pdf->Cell(120,6,utf8_decode('Descripcion'),1,0,'C',1);
   $pdf->Cell(120,6,utf8_decode('Reasignado a'),1,0,'C',1);
@@ -33,14 +23,12 @@
   $pdf->Ln(6);
   while ( $row = $resultado->fetch_assoc() )
   {
-  $pdf->SetX(180);
+  $pdf->SetX(30);
   $pdf->Cell(30,6,utf8_decode($row['numero_bien']),1,0,'C',0);  
   $pdf->Cell(120,6,utf8_decode($row['nombre_producto']),1,0,'C',0);
   $pdf->Cell(120,6,utf8_decode($row['asignacion_producto']),1,0,'C',0);
   $pdf->Cell(35,6,utf8_decode($row['precio_producto']),1,1,'C',0);  
   }
   $pdf->SetFont('Arial','',8);
-
-
   $pdf->Output();
 ?>
